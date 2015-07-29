1a0e4135e40b46b289f952d3a2e3d789f3e96383
Strohhalm <eder_armin@web.de>
2015-07-29 10:18:45 +0200

    Update NTypes.h

--- nbase/NTypes.h
+++ nbase/NTypes.h
@@ -1,4 +1,24 @@
 #ifndef NTYPES_H
 #define NTYPES_H
 
+typedef string NString;
+
+typedef char   nchar;
+typedef unsigned char nuchar;
+typedef short nshort;
+typedef unsigned short nushort;
+typedef int nint;
+typedef unsigned int nuint;
+typedef long nlong;
+typedef unsigned long nulong;
+typedef long long nlonglong;
+typedef unsigned long long nulonglong;
+typedef float nfloat;
+typedef double ndouble;
+typedef long double nlongdouble;
+typedef bool nbool;
+typedef size_t nsize;
+
+#define NXS(VALUE) NString(#VALUE)
+
 #endif
