0d9085af4410e1a353475ac44822691412695b4d
Strohhalm <eder_armin@web.de>
2015-07-30 08:21:15 +0200

    Update NServiceConnectionTcp.cpp

--- nservice/network/NServiceConnectionTcp.cpp
+++ nservice/network/NServiceConnectionTcp.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "NServiceConnectionTcp.h"
+
+namespace nox
+{
+    namespace service
+    {
+        namespace network
+        {
+            NServiceConnectionTcp::NServiceConnectionTcp(IINService * serivce, boost::asio::ip::tcp::socket * socket) : INConnectionTcp(socket)
+            {
+                m_Service = serivce;
+            }
+  
+            NServiceConnectionTcp::~NServiceConnectionTcp()
+            {
+            }
+  
+            void NServiceConnectionTcp::run()
+            {
+                _IINServiceResponse * response = NULL;
+  
+                boost::system::error_code errCode;
+                try
+                {
+                    while (((_IINServiceRequest *)getBuffer().c_array())->Head.Control.Size > getBytesRead())
+                        getSocket().receive(boost::asio::buffer(getBuffer()));
+  
+                    response = m_Service->call((_IINServiceRequest *) getBuffer().c_array(),
+                                               getBytesRead());
+                    if (response == NULL)
+                        throw NNullPointerException("Response is NULL");
+  
+                    boost::asio::write(*m_Socket,
+                                       boost::asio::buffer(response, response->Head.Control.Size),
+                                       errCode);
+                }
+                catch (exception & exp)
+                {
+                    NString message(exp.what());
+  
+                    if (response == NULL)
+                        response = new _IINServiceResponse();
+                    memset(response, 0, sizeof(_IINServiceResponse));
+  
+                    response->Head.Control.Size = sizeof(_IINServiceResponse);
+                    response->Head.Error.Code = 12;
+                    message.copy(response->Head.Error.Message, message.length());
+  
+                    boost::asio::write(*m_Socket,
+                                       boost::asio::buffer(response, response->Head.Control.Size),
+                                       errCode);
+                }
+                delete response;
+                delete this;
+            }
+        }
+    }
+}
